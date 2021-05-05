7c73c8478e2e1a47d30a7c5b84c74dbcb24706d0
goudete <goudetenrique@gmail.com>
2021-05-05 11:42:12 -0500

    bug fix on gt all users

--- routes/follow/getAllUsers.js
+++ routes/follow/getAllUsers.js
@@ -6,8 +6,9 @@ const knex = require('knex')(config);
 module.exports = async (req, res) => {
 
     try {
-        const users = await knex.select().from('user')
-        return res.status(200).json({ users })
+        const users = await knex.select().from('cluster_users');
+        console.log(users);
+        return res.status(200).json({ users });
     } catch (e) {
         console.log('error:', e)
         return res.status(500).json({
