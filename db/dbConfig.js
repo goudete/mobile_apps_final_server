312d55c7ab4511ade2656f480bc9a59b9c8fc592
goudete <goudetenrique@gmail.com>
2021-05-06 15:59:40 -0500

    knex

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,3 +1,6 @@
+const environment = process.env.ENVIRONMENT || 'development'
+const config = require('../../db/dbConfig')[environment];
+const knex = require('knex')(config);
 
 module.exports = {
     development: {
