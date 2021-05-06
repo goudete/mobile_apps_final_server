6857226c6f1b6958de1c317222cd9aed50e9f317
goudete <goudetenrique@gmail.com>
2021-05-06 15:52:10 -0500

    table creation script

--- routes/createDb/db.js
+++ routes/createDb/db.js
@@ -12,7 +12,7 @@ module.exports = (req, res) => {
   
     knex.schema.createTable('locations', (table) => {
       table.increments('id')
-      table.integer('user_id').references('id').inTable('user').notNull().onDelete('cascade');
+      table.integer('user_id').references('id').inTable('cluster_users').notNull().onDelete('cascade');
       table.string('name')
       table.string('google_place_id')
       table.string('description', 1000)
@@ -22,8 +22,8 @@ module.exports = (req, res) => {
   
     knex.schema.createTable('friend_connections', (table) => {
       table.increments('id')
-      table.integer('follower').references('id').inTable('user').notNull().onDelete('cascade');
-      table.integer('followee').references('id').inTable('user').notNull().onDelete('cascade');
+      table.integer('follower').references('id').inTable('cluster_users').notNull().onDelete('cascade');
+      table.integer('followee').references('id').inTable('cluster_users').notNull().onDelete('cascade');
     })
     .then(() => res.json({'success': 'table'}))
   
